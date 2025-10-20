7becf23a5f4512157ab1820c9be8aab72f640aab
ethicnology <ethicnology@pm.me>
2025-10-20 07:49:30 -0400

    feat: upgrade to v8

--- rust/src/ark/server_info.rs
+++ rust/src/ark/server_info.rs
@@ -1,24 +1,24 @@
-use anyhow::{Result};
+use anyhow::Result;
 use flutter_rust_bridge::frb;
 
 use crate::ark::client::ArkWallet;
 
-
-
 pub struct ServerInfo {
-    pub pk: String,
-    pub vtxo_tree_expiry: u32,
+    pub version: String,
+    pub signer_pubkey: String,
+    pub forfeit_pubkey: String,
+    pub forfeit_address: String,
+    pub checkpoint_tapscript: String,
+    pub network: String,
+    pub session_duration: i64,
     pub unilateral_exit_delay: u32,
     pub boarding_exit_delay: u32,
-    pub round_interval: i64,
-    pub network: String,
-    pub dust: i64,
-    pub forfeit_address: String,
-    pub version: String,
     pub utxo_min_amount: Option<i64>,
     pub utxo_max_amount: Option<i64>,
     pub vtxo_min_amount: Option<i64>,
     pub vtxo_max_amount: Option<i64>,
+    pub dust: i64,
+    pub digest: String,
 }
 
 impl ArkWallet {
@@ -26,19 +26,21 @@ impl ArkWallet {
     pub fn server_info(&self) -> Result<ServerInfo> {
         let info = self.inner.server_info.clone();
         Ok(ServerInfo {
-            pk: info.pk.to_string(),
-            vtxo_tree_expiry: info.vtxo_tree_expiry.0,
+            version: info.version,
+            signer_pubkey: info.signer_pk.to_string(),
+            forfeit_pubkey: info.forfeit_pk.to_string(),
+            forfeit_address: info.forfeit_address.to_string(),
+            checkpoint_tapscript: info.checkpoint_tapscript.to_string(),
+            network: info.network.to_string(),
+            session_duration: info.session_duration as i64,
             unilateral_exit_delay: info.unilateral_exit_delay.0,
             boarding_exit_delay: info.boarding_exit_delay.0,
-            round_interval: info.round_interval,
-            network: info.network.to_string(),
-            dust: info.dust.to_sat() as i64,
-            forfeit_address: info.forfeit_address.to_string(),
-            version: info.version,
             utxo_min_amount: info.utxo_min_amount.map(|amount| amount.to_sat() as i64),
             utxo_max_amount: info.utxo_max_amount.map(|amount| amount.to_sat() as i64),
             vtxo_min_amount: info.vtxo_min_amount.map(|amount| amount.to_sat() as i64),
             vtxo_max_amount: info.vtxo_max_amount.map(|amount| amount.to_sat() as i64),
+            dust: info.dust.to_sat() as i64,
+            digest: info.digest.to_string(),
         })
     }
 }
